ba83c4b233ed84dd6de52b62b9edfa702c4657af
oluwaseun adebanwo <findseun@gmail.com>
2021-05-19 14:29:55 +0100

    update checkout

--- components/Checkout.js
+++ components/Checkout.js
@@ -5,12 +5,22 @@ function Checkout() {
 
   useEffect(() => {
     let eventScript;
-    const event = document.getElementById("fola-event-script");
+    const eventCount = document.querySelectorAll(
+      "[id=fola-event-script]"
+    ).length;
+    // document.getElementById("fola-event-script");
 
-    if (event == null) {
+    console.log(eventCount);
+
+    if (scriptLoaded == false) {
+      console.log("creatScript");
+      // const script = document.createElement('script');
       eventScript = document.createElement("script");
+
       eventScript.setAttribute("async", ""); // Or defer or nothing
       eventScript.setAttribute("id", "fola-event-script");
+
+      // script.src = "source-of-script";
       eventScript.src =
         "https://www.eventbrite.com/static/widgets/eb_widgets.js";
       const position = document.querySelector("body"); // Or any other location , example head
@@ -19,22 +29,42 @@ function Checkout() {
       setScriptLoaded(true);
     }
 
-    //Check if window and script is loaded`
-    if (typeof window === undefined && !scriptLoaded) {
-      console.log("oh no");
-    } else {
+    if (typeof window != undefined && eventCount == 1 && scriptLoaded == true) {
       console.log("there you go");
-      window.EBWidgets.createWidget({
+      // const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+      // const vh = Math.max(
+      //   document.documentElement.clientHeight || 0,
+      //   window.innerHeight || 0
+      // );
+      const ebWidget = window.EBWidgets;
+
+      ebWidget.createWidget({
         // Required
         widgetType: "checkout",
         eventId: "155718792099",
         iframeContainerId: "eventbrite-widget-container-155718792099",
 
         // Optional
-        iframeContainerHeight: 425, // Widget height in pixels. Defaults to a minimum of 425px if not provided
+        iframeContainerHeight: 568, // vh, // Widget height in pixels. Defaults to a minimum of 425px if not provided
+
         onOrderComplete: callback, // Method called when an order has successfully completed
       });
     }
+
+    //This is not necessary but a check if script is required in some other part of the code`
+    // if (typeof window === undefined && !scriptLoaded) {
+
+    // }
+
+    // check if script is loaded
+    // if (scriptLoaded == true) {
+
+    // }
+
+    var callback = function () {
+      console.log("Order complete!");
+      // document.body.removeChild(eventScript);
+    };
   }, [scriptLoaded]);
 
   return <div id="eventbrite-widget-container-155718792099" />;
